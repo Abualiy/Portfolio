3c4a3ca6bb6df26d08d5d51785929ec6afc8cf11
Neima Nesru <neimanesru34@gmail.com>
2023-09-13 11:38:25 +0100

    removing unnecessary imports

--- codechat/src/pages/Home.jsx
+++ codechat/src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import Search from '../components/Search'
-import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
-import Chats from '../components/Chats'
-import Message from '../components/Message'
-import Input from '../components/Input'
 
 const Home = () => {
     return (
