8080dca313860e323094ff36e213183c5ef8b540
Akrem Muktar <akremmuktar332@gmail.com>
2023-09-19 21:10:52 +0300

    Done

--- codechat/src/context/AuthContext.js
+++ codechat/src/context/AuthContext.js
@@ -1,18 +1,24 @@
+// Import statements
 import { onAuthStateChanged } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase";
 
+// Create the AuthContext
 export const AuthContext = createContext();
 
+// AuthContextProvider component
 export const AuthContextProvider = ({ children }) => {
+    // State to hold the current user
     const [currentUser, setCurrentUser] = useState({});
 
+    // Effect to listen for changes in the authentication stat
     useEffect(() => {
+        // Subscribe to auth state changes
         const unsub = onAuthStateChanged(auth, (user) => {
             setCurrentUser(user);
-            console.log(user)
         });
 
+        // Unsubscribe from the auth state changes when the component unmounts
         return () => {
             unsub();
         }
@@ -20,6 +26,7 @@ export const AuthContextProvider = ({ children }) => {
     }, []);
 
     return (
+        // Provide the current user value to the context
         <AuthContext.Provider value={{ currentUser }}>
             {children}
         </AuthContext.Provider>
